84befb9c768ca15688543d8d38feba23d3e385e2
jchesson6 <jchesson6@gmail.com>
2025-02-26 00:42:59 -0500

    normalization, splitting, pytorch import, pre model design

--- database.py
+++ database.py
@@ -1,64 +1,95 @@
 import os
 import pandas as pd
 import pickle
+import datetime
 from playerobj import Player
 from basketball_reference_web_scraper import client
 from basketball_reference_web_scraper.data import OutputType, OutputWriteOption
+from sklearn.preprocessing import MinMaxScaler
 
 
-def update_player_database(season_end_year):
+def update_database(season_end_year=None):
+
+    if season_end_year == None:
+        season_end_year = datetime.datetime.now().year
 
     #create a file name using season
     #os.remove("2024_2025_player_season_totals.csv")
     season_start_year = season_end_year - 1
-    csv_file = f"./{season_start_year}_{season_end_year}_player_season_totals.csv"
-    database_file = f"{season_start_year}_{season_end_year}_player_database.dat"
+    total_csv_file = f"./{season_start_year}_{season_end_year}_player_season_totals.csv"
+    schedule_csv_file = f"./{season_start_year}_{season_end_year}_season_schedule.csv"
+    player_database_file = f"{season_start_year}_{season_end_year}_player_database.dat"
 
+    # pull season totals
     client.players_season_totals(
         season_end_year=season_end_year,
         output_type=OutputType.CSV, 
-        output_file_path=csv_file
+        output_file_path=total_csv_file
     )
 
-    totals_df = pd.read_csv(csv_file)
+    client.season_schedule(
+    season_end_year=season_end_year, 
+    output_type=OutputType.CSV, 
+    output_file_path=schedule_csv_file
+    )
+
+    #process the schedule file - add GAME PLAYED, CHANGE DATES, 
 
     #create player database with slug, Name - Team - Position
     # will need to handle duplicated players on basketball reference that have switched teams somehow
+    totals_df = pd.read_csv(total_csv_file)
     player_list = []
 
     for row in totals_df.itertuples():
         player = Player(row.slug, row.name, row.team, row.positions)
         player_list.append(player)
         # will probably implement pulling the box scores for each player and combining them into a single dataframe and csv
+        # could possibly add the season schedule as a player object property
     
     # save database as a pickle file
-    with open(database_file, 'wb') as database:
-        pickle.dump(player_list, database)
+    with open(player_database_file, 'wb') as player_database:
+        pickle.dump(player_list, player_database)
 
     return player_list
    
 
-def return_current_player_list(name):
+def return_current_player_list(season_end_year=None):
 
-    return update_player_database(2025)
+    if season_end_year == None:
+        season_end_year = datetime.datetime.now().year
+
+    season_start_year = season_end_year - 1
+    player_database_file = f"{season_start_year}_{season_end_year}_player_database.dat"
+
+    with open(player_database_file, 'rb') as player_database:
+        player_list = pickle.load(player_database)
+
+    return player_list
 
 
 
-def get_player_box_scores(slug, season_end_year, filename, process=True):
+def get_player_box_scores(player, season_end_year=None, process=True):
 
     #create file name using player name and season if storing all csvs
+    if season_end_year == None:
+        season_end_year = datetime.datetime.now().year
+
+    season_start_year = season_end_year - 1
+    player_box_file = f"./{season_start_year}_{season_end_year}_{player.name_as_file()}_reg_season_box_scores.csv"
 
     client.regular_season_player_box_scores(
-        player_identifier=slug, 
+        player_identifier=player.slug, 
         season_end_year=season_end_year, 
         output_type=OutputType.CSV, 
-        output_file_path=filename
+        output_file_path=player_box_file
     )
     #this will probably move into the update database and be replaced with reading a large csv
 
     #create to date ppg, rpg, apg, etc stats
     if process:
-        process_box_scores(filename)
+        process_box_scores(player_box_file)
+
+    #potentially create a data quality report (ECE5464 slide 5)
 
     #add to the overall csv containing all player box scores
 
@@ -69,9 +100,20 @@ def process_box_scores(filename):
 
     # may need to remove inactive games but currently seems like they are already removed
 
+    # convert seconds to minutes
+    minutes = orig_df['seconds_played'].div(60.0)
+    orig_df.insert(11, 'minutes_played', minutes)
+
     # add rebounds together to get full rebounds per game
     rebounds = orig_df['offensive_rebounds'] + orig_df['defensive_rebounds']
     orig_df.insert(17, 'total_rebounds', rebounds)
+
+    # perform normalization and data preparation
+
+    # - 1-hot encode team and probably opponent
+
+    # - binary encode active status and outcome
+
 
     # calculate fantasy points for each game
     orig_df['fpts'] = orig_df.apply(get_fantasy_points, axis=1)
@@ -96,36 +138,45 @@ def get_fantasy_points(row):
             )
 
 
-def create_per_game_stats():
 
-    total_df = pd.read_csv(filepath_or_buffer="2024_2025_player_season_totals.csv")
-    total_df['rebounds'] = total_df['offensive_rebounds'] + total_df['defensive_rebounds']
-    
-    per_game_df = total_df[['slug', 'name', 'positions', 'age', 'team']].copy()
+def normalize_and_split_box(player, season_end_year=None, target="points"):
 
-    per_game_df['PPG'] = total_df['points'].div(total_df['games_played'])
-    per_game_df['RPG'] = total_df['rebounds'].div(total_df['games_played'])
-    per_game_df['APG'] = total_df['assists'].div(total_df['games_played'])
-    per_game_df['SPG'] = total_df['steals'].div(total_df['games_played'])
-    per_game_df['BPG'] = total_df['blocks'].div(total_df['games_played'])
-    per_game_df['TOPG'] = total_df['turnovers'].div(total_df['games_played'])
+    if season_end_year == None:
+        season_end_year = datetime.datetime.now().year
 
-    per_game_df.to_csv(path_or_buf="2024_2025_player_season_totals_per_game.csv")
+    season_start_year = season_end_year - 1
+    player_box_file = f"./{season_start_year}_{season_end_year}_{player.name_as_file()}_reg_season_box_scores.csv"
 
+    df = pd.read_csv(player_box_file)
 
-def split_box_data(filename, cleaned_filename):
-
-    df = pd.read_csv(filename)
+    scaler = MinMaxScaler()
     
     # remove unnecessary column
     # - dont know if fantasy points are useful right now
     # removing all string columns to just test on stats first. Opponent information and extra factors will be added later.
-    x = df.drop(columns=['active', 'plus_minus', 'team', 'location', 'opponent', 'outcome', 'game_score', 'fpts'])
+    #x = df.drop(columns=['active', 'plus_minus', 'team', 'location', 'opponent', 'outcome', 'game_score', 'fpts'])
 
-    # convert string column to numerical
 
     # split into inputs and outputs
-    y = df[['points_scored', 'total_rebounds', 'assists', 'steals', 'blocks', 'turnovers', 'made_field_goals',
-             'attempted_field_goals', 'made_three_point_field_goals']]
-    
-    x.to_csv(cleaned_filename)
+    # output for a multioutput model (NOT USING RIGHT NOW)
+    # y = df[['points_scored', 'total_rebounds', 'assists', 'steals', 'blocks', 'turnovers', 'made_field_goals',
+           # 'attempted_field_goals', 'made_three_point_field_goals']]
+
+    # customize data based on desired output
+    if target == "total_rebounds":
+        y = df['total_rebounds']
+    elif target == "assists":
+        y = df['assists']
+    else:
+        df = df[['seconds_played', 'minutes_played','made_field_goals', 'attempted_field_goals', 'made_three_point_field_goals', 
+                'attempted_three_point_field_goals', 'made_free_throws', 'attempted_free_throws', 'ppg_td', 'points_scored']]
+        
+        x = df.drop(columns=['points_scored'])
+        x_labels = x.columns.to_list()
+        y = df[['points_scored']]
+        y_labels = y.columns.to_list()
+
+        x_t = scaler.fit_transform(x,y)
+        y_t = scaler.fit_transform(y)
+
+    return x_t, y_t, x_labels, y_labels
--- main.py
+++ main.py
@@ -1,35 +1,45 @@
 import os
-import pandas as pd
-import datetime
-from database import return_current_player_list, get_player_box_scores, split_box_data
+import datetime, time
+from database import update_database, return_current_player_list, get_player_box_scores, normalize_and_split_box
 from InquirerPy import inquirer
-from sklearn import tree
+from sklearn import tree, pipeline
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.linear_model import Ridge
 
 
+
+# update database
+#today = datetime.date.today()
+#cur_year = today.year
+
+#set an if condition or automate this in a new script
+update_database()
+player_list = return_current_player_list()
 
 # search to find player (Inquirer)
-
 player = inquirer.fuzzy(
     message="Select current player",
-    choices=return_current_player_list,
+    choices=player_list,
     match_exact=True,
 ).execute()
 
-# update database
+# select which outputs to predict
 
-# get_player_box_scores("gilgesh01", 2024,"./2023_2024_shai_regular_season_box_scores.csv")
+# pick a game from the schedule to estimate for
 
-today = datetime.date.today()
-cur_year = today.year
 
-player_box_file = f"./{cur_year}_{player.name_as_file()}_reg_season_box_scores.csv"
+#player_box_file = f"./{cur_year}_{player.name_as_file()}_reg_season_box_scores.csv"
 #processed_file_2024_2025_shai = "./processed_2024_2025_shai_regular_season_box_scores.csv"
-get_player_box_scores(player.slug, cur_year, player_box_file)
-#split_box_data(file_2024_2025_shai, processed_file_2024_2025_shai)
+
+#get player box score
+get_player_box_scores(player)
+X, y, feature_labels, target_label = normalize_and_split_box(player)
+print(X[:3])
+print(y[:3])
 
 # train on previous seasons and test on current
 
 
-# decision tree for feature selection
-tree_clf = tree.DecisionTreeRegressor()
+# will probably go with multiple models for each predicted stat
+
 
